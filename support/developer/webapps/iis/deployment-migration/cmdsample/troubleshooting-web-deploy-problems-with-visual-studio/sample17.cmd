200149c6c7b0d0728907d7d77e5d0b877a29bcdc
sevend2 <77822529+sevend2@users.noreply.github.com>
2023-06-09 15:19:11 +0800

    Add files via upload

--- support/developer/webapps/iis/deployment-migration/cmdsample/troubleshooting-web-deploy-problems-with-visual-studio/sample17.cmd
+++ support/developer/webapps/iis/deployment-migration/cmdsample/troubleshooting-web-deploy-problems-with-visual-studio/sample17.cmd
@@ -1,2 +1,5 @@
-Make sure the site name, user name, and password are correct. If the issue is not resolved,
-please contact your local or server administrator.
+Error details:
+Connected to the destination computer ("deployserver") using the Web Management Service,
+but could not authorize. Make sure that you are using the correct user name and password,
+that the site you are connecting to exists, and that the credentials represent a user who
+has permissions to access the site.
